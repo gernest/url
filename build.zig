03f33e68f2bf989d6eea629b6aab270ea01a560f
gernest <geofreyernest@live.com>
2019-10-21 22:40:57 +0300

    add mod.zig

--- build.zig
+++ build.zig
@@ -1,12 +1,6 @@
 const Builder = @import("std").build.Builder;
+const mod = @import("mod.zig");
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-
-    var main_tests = b.addTest("src/url_test.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-    b.default_step.dependOn(test_step);
+    mod.build(b);
 }
